addb786927811836ed2ed0ee79015540de8290a6
Kyle Lin <kylejlin.us@gmail.com>
2018-05-20 20:30:02 -0700

    Correct things

---
+++ bootstrap.js
@@ -0,0 +1,3 @@
+import('./index').then(() => {
+  console.log('Loaded index.js.');
+});
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { add_one } from './wasm_rust_quickstart'
+import { add_one } from './wasm_rust_quickstart';
 
-document.getElementsByTagName('p').textContent += ` 42 + 1 = ${add_one(42)}`;
+document.getElementsByTagName('p')[0].textContent += ` 42 + 1 = ${add_one(42)}`;
 
 // Congratulations!
 // You just performed that mind-boggling computation with a Rust function :)
